a650a178f1cb8aaa338f1f2e8fa9dd97189928ac
MrSingh0011 <rishabh.singh@jbmgroup.com>
2024-04-23 17:08:07 +0530

    report

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -2,26 +2,18 @@ import React from 'react';
 import Logout from '../Logout';
 import { Link } from 'react-router-dom';
 
-const Header = ({setIsAdding,setShowReport,isReportPage}) => {
-function handleST(){
-  setIsAdding(true) 
-  setShowReport(false)
-}
+const Header = ({ setIsAdding, setShowReport, isReportPage }) => {
 
-function handleR(){
-  setShowReport(true) 
-  setIsAdding(false)
-}
   return (
     <header className=''>
       <div className='d-flex justify-content-between px-5 py-2 align-self-center header'>
-        <div className='my-auto'style={{ fontSize: 25 }}>Training Module</div>
+        <div className='my-auto' style={{ fontSize: 25 }}>Training Module</div>
         <div className='d-flex gap-3'>
-        <Link to={"/"} ><button className='btn-schedule'>Training List</button></Link>
+          <Link to={"/"} ><button className='btn-schedule'>Training List</button></Link>
           <Link to={"/schedule"} ><button className='btn-schedule'>Schedule Training</button></Link>
-         
-            <Link to={"/reports"}><button  className='btn-schedule'>Report</button></Link>
-          
+
+          <Link to={"/reports"}><button className='btn-schedule'>Report</button></Link>
+
           <Logout />
         </div>
       </div>
